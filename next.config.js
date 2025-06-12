944bf42b9cb5644b29d770337f64aab2f557a510
Nylon1 <71017473+Nylon1@users.noreply.github.com>
2025-06-12 22:23:31 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 
-
-
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    middleware: true,
-  },
 };
 
 module.exports = nextConfig;
